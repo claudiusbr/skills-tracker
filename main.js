3a63e1344af5f98e7dae14803abb76800b3203a8
Claudius Brasil <claudius.brasil@kry.se>
2022-04-08 22:14:21 +0100

    refactor js code into own file

---
+++ main.js
@@ -0,0 +1,42 @@
+// this stops the form from refreshing the page after being submitted
+document
+  .getElementById("myform")
+  .addEventListener("submit", (e) => e.preventDefault());
+
+// TODO: sanitise input
+// TODO: make sure duplicate skills are not allowed on list
+// TODO: plus button that shows the text input form
+// TODO: little bit of css
+
+// this function takes form inputs and manipulates the dom with them
+const populateFieldsOnSubmit = (formElement) => {
+  const skillName = formElement.name.value;
+  createListItem(skillName);
+};
+
+const deleteListItem = (formElement) => {
+  formElement.remove();
+};
+
+const getIdFromUserInput = (userInput) =>
+  userInput.toLowerCase().replaceAll(/\W/g, "");
+
+const createListItem = (skillName) => {
+  const ul = document.getElementById("list");
+  const li = document.createElement("li");
+
+  const skillId = getIdFromUserInput(skillName);
+  const removeSkillButton = document.createElement("button");
+  const skillSpan = document.createElement("span");
+
+  removeSkillButton.addEventListener("click", (event) =>
+    document.getElementById(skillId).remove()
+  );
+
+  removeSkillButton.appendChild(document.createTextNode("-"));
+  skillSpan.appendChild(document.createTextNode(skillName));
+  li.setAttribute("id", skillId);
+  li.appendChild(skillSpan);
+  li.appendChild(removeSkillButton);
+  ul.appendChild(li);
+};
