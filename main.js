a1cab21d6cea4d503038d2a62d55cb2536cec164
Claudius Brasil <claudius.brasil@kry.se>
2022-04-24 20:15:21 +0100

    add dummy dropdown sklll selector box

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 // this stops the form from refreshing the page after being submitted
 document
-  .getElementById("myform")
+  .getElementById("my-form")
   .addEventListener("submit", (e) => e.preventDefault());
 
-// TODO: sanitise input
 // TODO: make sure duplicate skills are not allowed on list
 // TODO: plus button that shows the text input form
 // TODO: little bit of css
 
-// this function takes form inputs and manipulates the dom with them
+// TODO: sanitise formElement input
 const addSkill = (formElement) => {
   const skillName = formElement.name.value;
   if (skillName) {
@@ -19,7 +18,7 @@ const addSkill = (formElement) => {
 const getIdFromUserInput = (userInput) =>
   userInput.toLowerCase().replaceAll(/\W/g, "");
 
-const clearInputField = () => document.getElementById("myform").reset();
+const clearInputField = () => document.getElementById("my-form").reset();
 
 const createListItem = (skillName) => {
   const ul = document.getElementById("list");
@@ -29,13 +28,31 @@ const createListItem = (skillName) => {
   const removeSkillButton = document.createElement("button");
   const skillSpan = document.createElement("span");
 
-  removeSkillButton.addEventListener("click", (event) => li.remove());
+  removeSkillButton.addEventListener("click", (e) => li.remove());
 
   removeSkillButton.appendChild(document.createTextNode("-"));
   skillSpan.appendChild(document.createTextNode(skillName));
   li.setAttribute("id", skillId);
   li.appendChild(skillSpan);
+  li.appendChild(createSkillSelector(skillId));
   li.appendChild(removeSkillButton);
   ul.appendChild(li);
   clearInputField();
+};
+
+// TODO sanitise skillId input
+const createSkillSelector = (skillId) => {
+  const dropDown = document.createElement("select");
+  dropDown.name = "skill-level-selector";
+  dropDown.id = "skill-level-selector-" + skillId;
+
+  ["Novice", "Beginner", "Competent", "Proficient", "Expert"].forEach(
+    (level) => {
+      const skillLevelOption = document.createElement("option");
+      skillLevelOption.value = level.toLowerCase();
+      skillLevelOption.innerHTML = level;
+      dropDown.add(skillLevelOption);
+    }
+  );
+  return dropDown;
 };
